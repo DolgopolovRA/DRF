bd5b8abd8c5df0856679636c6ed92970d7d36970
DolgopolovRA <ra.dolgopolov@yandex.ru>
2023-01-19 16:07:55 +0300

    dz_4

--- TODO/todoapp/views.py
+++ TODO/todoapp/views.py
@@ -2,13 +2,36 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import ToDo, Project
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from .filters import ProjectFilter
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    filterset_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
+
+    def destroy(self, request, *args, **kwargs):
+        obj = self.get_object()
+        obj.is_active = False
+        obj.save()
+        return Response()
+
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    filterset_fields = ['project']
+    pagination_class = ToDoLimitOffsetPagination
--- TODO/users/views.py
+++ TODO/users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework import mixins
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(GenericViewSet, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+
